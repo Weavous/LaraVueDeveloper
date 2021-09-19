a5fec382a1c4b1a8cfab0c68e2cc544b8251eb56
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-09-19 16:47:23 -0300

    Modify Developer Model

--- server/app/Models/Developer.php
+++ server/app/Models/Developer.php
@@ -6,17 +6,27 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 use App\Models\Hobby;
+use App\Models\Tool;
+use App\Models\DeveloperHobby;
+use App\Models\DeveloperTool;
 
 class Developer extends Model
 {
     use HasFactory;
 
-    protected $fillable = ["name", "sex", "birthdate", "hobby_id"];
+    protected $fillable = ['name', 'sex', 'birthdate', 'hobby_id'];
 
-    public function hobby(): BelongsTo
+    public function hobbies(): BelongsToMany
     {
-        return $this->belongsTo(Hobby::class);
+        return $this->belongsToMany(Hobby::class, DeveloperHobby::class);
+    }
+
+    public function tools(): BelongsToMany
+    {
+        return $this->BelongsToMany(Tool::class, DeveloperTool::class);
     }
 }
